715a5945c969ee996a914eadcf079347720a7ce8
sam-lb <samlbrunacini@gmail.com>
2022-05-11 16:38:46 -0400

    added templates for views and updated styling

--- script_1.js
+++ script_1.js
@@ -16,4 +16,10 @@ var layout = {
   title:'graph of data will go here'
 };
 
-Plotly.newPlot('chart-div-1', data, layout);
+// to be replaced with actual data
+tickers = ["ticker 1", "ticker 2", "ticker 3"];
+index_funds = ["index 1", "index 2", "index 3"];
+
+
+
+// Plotly.newPlot('chart-div-1', data, layout);
